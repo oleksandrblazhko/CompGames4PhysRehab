5f93cee187b1fdf12448c6a58c1b0c3bf8d51e77
asanvolkov <130404650+asanvolkov@users.noreply.github.com>
2025-09-21 03:51:57 +0300

    Update db_manager.py

--- PythonTurbo/db_manager.py
+++ PythonTurbo/db_manager.py
@@ -4,7 +4,6 @@ from datetime import datetime
 DB_NAME = "sensors.db"
 
 def init_db():
-    """Створює бази даних і таблиці, якщо їх нема"""
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS sensor_data
@@ -16,6 +15,7 @@ def init_db():
                        time TIMESTAMP NOT NULL)''')
     cursor.execute('''CREATE TABLE IF NOT EXISTS calibration
                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       session_id TEXT NOT NULL,
                        calib_x REAL NOT NULL,
                        calib_y REAL NOT NULL,
                        time TIMESTAMP NOT NULL)''')
@@ -23,7 +23,6 @@ def init_db():
     conn.close()
 
 def insert_sensor_data(session_id, x, y, z):
-    """Вставляє дані з Arduino в таблицю sensor_data"""
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     cursor.execute('INSERT INTO sensor_data (session_id, x, y, z, time) VALUES (?,?,?,?,?)',
@@ -31,11 +30,10 @@ def insert_sensor_data(session_id, x, y, z):
     conn.commit()
     conn.close()
 
-def insert_calibration(calib_x, calib_y):
-    """Зберігає значення калібровки в таблицю calibration"""
+def insert_calibration(calib_x, calib_y, session_id):
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO calibration (calib_x, calib_y, time) VALUES (?,?,?)',
-                   (calib_x, calib_y, datetime.now().isoformat()))
+    cursor.execute('INSERT INTO calibration (session_id, calib_x, calib_y, time) VALUES (?,?,?,?)',
+                   (session_id, calib_x, calib_y, datetime.now().isoformat()))
     conn.commit()
     conn.close()
